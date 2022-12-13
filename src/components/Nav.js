129b7c30dea162009f670bc13049b2baaec0eb84
jyu-1 <jiabids@gmail.com>
2022-12-13 07:47:52 -0800

    change nav bar styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,17 +4,35 @@ const Nav = (props) => {
     return (
         <nav>
             <Link to="/">
-                <div>LAUREN</div>
+                <div className="logo">LAUREN</div>
             </Link>
             <ul className="nav-links">
                 <Link to="/shop">
-                    <li>Shop</li>
+                    <li>SHOP</li>
                 </Link>
                 <Link to="/cart">
-                    <li>Cart [{props.itemCount}]</li>
+                    <li className="cart-align">
+                        <CartIcon />
+                        <span className="cart-number">{props.itemCount}</span>
+                    </li>
                 </Link>
             </ul>
         </nav>
+    );
+};
+
+const CartIcon = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="currentColor"
+            className="bi bi-bag"
+            viewBox="0 0 16 16"
+        >
+            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+        </svg>
     );
 };
 
